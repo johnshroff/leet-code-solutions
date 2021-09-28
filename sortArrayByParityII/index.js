5357aa56be4826cf901befe5fc1f9bd112a7b8e7
John Shroff <john.shroff@flowtrac.com>
2021-09-28 09:58:26 -0500

    add sortArrayByParityII

--- sortArrayByParityII/index.js
+++ sortArrayByParityII/index.js
@@ -1,4 +1,7 @@
 /**
+ * Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+ * Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+ * {@link https://leetcode.com/problems/sort-array-by-parity-ii/}
  * @param {number[]} nums
  * @return {number[]}
  */
