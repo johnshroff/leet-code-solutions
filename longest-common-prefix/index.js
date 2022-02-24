1b3887a8d415c2cc4fc56062205c59943d7c8ad8
John Shroff <John.Shroff@experian.com>
2022-02-24 10:01:25 -0600

    longest-common-prefix

---
+++ longest-common-prefix/index.js
@@ -0,0 +1,28 @@
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    const returnString = [];
+
+    let matches = true;
+    let x = 0;
+
+    const charMatches = (chr, x) => {
+        for (let y = 1; y < strs.length; ++y) {
+            if (strs[y][x] !== chr) {
+                return false;
+            }
+        }
+        return true;
+    };
+    do {
+        matches = charMatches(strs[0][x], x);
+        if (matches) {
+            returnString.push(strs[0][x]);
+        }
+    } while(matches && strs[0][++x] !== undefined);
+    return returnString.join("");
+};
